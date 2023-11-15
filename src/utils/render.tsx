a8c9400db2bb5b599740f1e336a24bb4a0371337
Peter Coolen <info@petercoolen.com>
2023-11-15 21:36:40 +0100

    plugin

--- src/utils/render.tsx
+++ src/utils/render.tsx
@@ -1,9 +1,9 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-
 export function render(component: React.ReactNode): void {
-  root.render(<StrictMode>{component}</StrictMode>);
+  const element = document.getElementById('root');
+  const root = element && createRoot(element);
+
+  root?.render(<StrictMode>{component}</StrictMode>);
 }
