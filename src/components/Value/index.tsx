c8db0aef36d864a2a85ef52e2ac636f6d50f9c8a
Peter Coolen <info@petercoolen.com>
2023-11-29 09:22:51 +0100

    value

--- src/components/Value/index.tsx
+++ src/components/Value/index.tsx
@@ -1,17 +1,17 @@
-import { useCallback, useEffect, useMemo } from 'react';
-import { normalizeConfig } from '../../types';
-import Price from '../Price';
-import { useCtx } from 'datocms-react-ui';
-import { RenderFieldExtensionCtx } from 'datocms-plugin-sdk';
-import ShopifyClient from '../../utils/ShopifyClient';
-import useStore, { State } from '../../utils/useStore';
-import s from './styles.module.css';
-import classNames from 'classnames';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useCallback, useEffect, useMemo } from "react";
+import { normalizeConfig } from "../../types";
+import Price from "../Price";
+import { useCtx } from "datocms-react-ui";
+import { RenderFieldExtensionCtx } from "datocms-plugin-sdk";
+import ShopifyClient from "../../utils/ShopifyClient";
+import useStore, { State } from "../../utils/useStore";
+import s from "./styles.module.css";
+import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faExternalLinkAlt,
   faTimesCircle,
-} from '@fortawesome/free-solid-svg-icons';
+} from "@fortawesome/free-solid-svg-icons";
 
 export type ValueProps = {
   value: string;
@@ -22,46 +22,50 @@ export default function Value({ value, onReset }: ValueProps) {
   const ctx = useCtx<RenderFieldExtensionCtx>();
 
   const { storefrontAccessToken, shopifyDomain } = normalizeConfig(
-    ctx.plugin.attributes.parameters,
+    ctx.plugin.attributes.parameters
   );
 
   const client = useMemo(
     () => new ShopifyClient({ shopifyDomain, storefrontAccessToken }),
-    [storefrontAccessToken, shopifyDomain],
+    [storefrontAccessToken, shopifyDomain]
   );
 
   const { product, status } = useStore(
-    useCallback((state) => (state as State).getProduct(value), [value]),
+    useCallback((state) => (state as State).getProduct(value), [value])
   );
 
-  const fetchProductByHandle = useStore(
-    (state) => (state as State).fetchProductByHandle,
+  // const fetchProductByHandle = useStore(
+  //   (state) => (state as State).fetchProductByHandle,
+  // );
+
+  const fetchProductById = useStore(
+    (state) => (state as State).fetchProductById
   );
 
   useEffect(() => {
-    fetchProductByHandle(client, value);
-  }, [client, value, fetchProductByHandle]);
+    fetchProductById(client, value);
+  }, [client, value, fetchProductById]);
 
   return (
     <div
-      className={classNames(s['value'], {
-        [s['loading']]: status === 'loading',
+      className={classNames(s["value"], {
+        [s["loading"]]: status === "loading",
       })}
     >
-      {status === 'error' && (
-        <div className={s['product']}>
+      {status === "error" && (
+        <div className={s["product"]}>
           API Error! Could not fetch details for product:&nbsp;
           <code>{value}</code>
         </div>
       )}
       {product && (
-        <div className={s['product']}>
+        <div className={s["product"]}>
           <div
-            className={s['product__image']}
+            className={s["product__image"]}
             style={{ backgroundImage: `url(${product.imageUrl})` }}
           />
-          <div className={s['product__info']}>
-            <div className={s['product__title']}>
+          <div className={s["product__info"]}>
+            <div className={s["product__title"]}>
               <a
                 href={product.onlineStoreUrl}
                 target="_blank"
@@ -71,18 +75,18 @@ export default function Value({ value, onReset }: ValueProps) {
               </a>
               <FontAwesomeIcon icon={faExternalLinkAlt} />
             </div>
-            <div className={s['product__description']}>
+            <div className={s["product__description"]}>
               {product.description}
             </div>
             {product.productType && (
-              <div className={s['product__producttype']}>
+              <div className={s["product__producttype"]}>
                 <strong>Product type:</strong>
                 &nbsp;
                 {product.productType}
               </div>
             )}
 
-            <div className={s['product__price']}>
+            <div className={s["product__price"]}>
               <strong>Price:</strong>
               &nbsp;
               {product.priceRange.maxVariantPrice.amount !==
@@ -99,7 +103,7 @@ export default function Value({ value, onReset }: ValueProps) {
           </div>
         </div>
       )}
-      <button type="button" onClick={onReset} className={s['reset']}>
+      <button type="button" onClick={onReset} className={s["reset"]}>
         <FontAwesomeIcon icon={faTimesCircle} />
       </button>
     </div>
